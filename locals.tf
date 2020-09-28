b1d09b7d1b522696c9ea44be71372f0a97a7a836
Kumar Rajhansh <krajhansh@quinovas.com>
2020-09-28 16:18:01 +0530

    few changes

--- locals.tf
+++ locals.tf
@@ -1,11 +1,11 @@
-locals {
+/*locals {
   aws_cli_command          = var.install_aws_cli ? local.aws_module_cli_command : local.aws_standard_cli_command
   aws_module_cli_command   = "${path.module}/awscli/bin/aws"
   aws_standard_cli_command = "aws"
-}
-
-/*locals {
-  aws_cli_command                           = var.install_aws_cli ? local.aws_standard_cli_command : local.aws_module_cli_command
-  aws_module_cli_command                    = "${path.module}/awscli/bin/aws"
-  aws_standard_cli_command                  = "aws"
 }*/
+
+locals {
+  aws_cli_command                           = var.install_aws_cli ? local.aws_standard_cli_command : local.aws_module_cli_command
+  aws_module_cli_command                    = "aws"
+  aws_standard_cli_command                  = "~/bin/aws"
+}
