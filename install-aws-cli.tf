9d1ac48a258684eacad89fba37ed5790337be8e3
Kumar Rajhansh <krajhansh@quinovas.com>
2020-09-28 16:10:43 +0530

    removed sudo

--- install-aws-cli.tf
+++ install-aws-cli.tf
@@ -4,7 +4,7 @@ resource "null_resource" "install_aws_cli" {
     provisioner "local-exec" {
     command = <<EOH
 
-sudo curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "${path.module}/awscli-bundle.zip" \
+curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "${path.module}/awscli-bundle.zip" \
 && unzip ${path.module}/awscli-bundle.zip -d ${path.module} \
 && ${path.module}/awscli-bundle/install -i ${path.module}/awscli
 EOH 
