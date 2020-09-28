e237722fe49d6cd6bf71e6d5348614ccd3e425e6
Kumar Rajhansh <krajhansh@quinovas.com>
2020-09-28 16:37:26 +0530

    few more changes

--- install-aws-cli.tf
+++ install-aws-cli.tf
@@ -1,16 +1,8 @@
 
 resource "null_resource" "install_aws_cli" {
   count = var.install_aws_cli ? 1 : 0
-    /*provisioner "local-exec" {
-    command = <<EOH
 
-curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "${path.module}/awscli-bundle.zip" \
-&& unzip ${path.module}/awscli-bundle.zip -d ${path.module} \
-&& ${path.module}/awscli-bundle/install -i ${path.module}/awscli
-EOH 
-
-  }*/
-    provisioner "local-exec" {
+  provisioner "local-exec" {
     command = <<EOH
     curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "${path.module}/awscli-bundle.zip"; unzip ${path.module}/awscli-bundle.zip -d ${path.module}; ${path.module}/awscli-bundle/install -b ~/bin/aws;
 EOH
