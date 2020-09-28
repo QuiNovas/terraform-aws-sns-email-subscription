3a489a5b40016b29721debd0669180119d96f99f
Kumar Rajhansh <krajhansh@quinovas.com>
2020-09-28 15:48:58 +0530

    typo

--- locals.tf
+++ locals.tf
@@ -1,9 +1,9 @@
-/*locals {
+locals {
   aws_cli_command          = var.install_aws_cli ? local.aws_module_cli_command : local.aws_standard_cli_command
   aws_module_cli_command   = "${path.module}/awscli/bin/aws"
   aws_standard_cli_command = "aws"
-}*/
-locals {
+}
+/*locals {
   aws_cli_command                           = var.install_aws_cli ? local.aws_standard_cli_command : local.aws_module_cli_command
   aws_module_cli_command                    = "${path.module}/awscli/bin/aws"
   aws_standard_cli_command                  = "aws"
