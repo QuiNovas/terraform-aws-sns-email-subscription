7e5419c0a8b958ea42f89131990abcb017057bad
krajhansh <krajhansh@quinovas.com>
2021-01-20 21:20:44 +0530

    removed aws cli

--- locals.tf
+++ locals.tf
@@ -1,5 +1,3 @@
 locals {
-  aws_cli_command          = var.install_aws_cli ? local.aws_standard_cli_command : local.aws_module_cli_command
-  aws_module_cli_command   = "aws"
-  aws_standard_cli_command = "~/bin/aws"
+  aws_cli_command          = "aws"
 }
